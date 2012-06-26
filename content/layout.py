0191cdd55d7e6f1c6cdfd419984ac0f2c803c9bc
roger <igor.onyshchenko@gmail.com>
2012-06-26 17:34:25 +0300

    i

--- content/layout.py
+++ content/layout.py
@@ -11,7 +11,7 @@ def getenv():
     
     if env is None:
         env = Environment(loader = FileSystemLoader(cherrypy.request.app.config["ukrainianside"]["base_dir"] + \
-            "content\pages"))
+            "content"))
         
     return env
 
@@ -19,41 +19,41 @@ def getIndex():
     return getHome()
     
 def getHome():
-    tmpl = getenv().get_template("home.html")
+    tmpl = getenv().get_template("pages/home.html")
     return tmpl.render()
 
 def getOneDayInOdessa():
-    tmpl = getenv().get_template("one-day-in-odessa.html")
+    tmpl = getenv().get_template("pages/one-day-in-odessa.html")
     return tmpl.render()
 
 def getTripToOlbia():
-    tmpl = getenv().get_template("trip-to-olbia.html")
+    tmpl = getenv().get_template("pages/trip-to-olbia.html")
     return tmpl.render()
 
 def getOdessaRegionTrainsTimetable():
-    tmpl = getenv().get_template("odessa-region-trains-timetable.html")
+    tmpl = getenv().get_template("pages/odessa-region-trains-timetable.html")
     return tmpl.render()
 
 def getIntroduction():
-    tmpl = getenv().get_template("introduction.html")
+    tmpl = getenv().get_template("pages/introduction.html")
     return tmpl.render()
 
 def getNationalArchaeologicalParkOlbia():
-    tmpl = getenv().get_template("national-archaeological-park-olbia.html")
+    tmpl = getenv().get_template("pages/national-archaeological-park-olbia.html")
     return tmpl.render()
 
 def getTowns():
-    tmpl = getenv().get_template("towns.html")
+    tmpl = getenv().get_template("pages/towns.html")
     return tmpl.render()
 
 def getInformation():
-    tmpl = getenv().get_template("information.html")
+    tmpl = getenv().get_template("pages/information.html")
     return tmpl.render()
 
 def getGeneral():
-    tmpl = getenv().get_template("general.html")
+    tmpl = getenv().get_template("pages/general.html")
     return tmpl.render()
 
 def getGreek():
-    tmpl = getenv().get_template("greek.html")
+    tmpl = getenv().get_template("pages/greek.html")
     return tmpl.render()
