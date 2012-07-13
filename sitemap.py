f64924c62c79c86d5b3a1d7f19ac9c652fa905a7
roger <igor.onyshchenko@gmail.com>
2012-07-13 10:32:17 +0300

    i

--- sitemap.py
+++ sitemap.py
@@ -4,7 +4,7 @@ from data import urls
 
 
 def getSitemap():
-    now = str(datetime.datetime.now())
+    now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z")
     
     sitemap = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" \
         "<urlset xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\" xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">"\
