84a66e96361126d9221f2d20f00f1148f3389b3d
roger <igor.onyshchenko@gmail.com>
2012-08-22 10:17:54 +0300

    i

--- data/aliases.py
+++ data/aliases.py
@@ -3,7 +3,8 @@ import cherrypy
 
 
 def getAll():
-    conn = sqlite3.connect(cherrypy.request.app.config["ukrainianside"]["base_dir"] + "data/data.db")
+    #conn = sqlite3.connect(cherrypy.request.app.config["ukrainianside"]["base_dir"] + "data/data.db")
+    conn = sqlite3.connect('/home/www/ukrainianside/' + "data/data.db")
     cur = conn.cursor()
         
     cur.execute("select value from aliases;")
