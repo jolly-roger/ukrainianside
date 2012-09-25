c8ede8d92d1be007e9aa401a263bfd490cb6adcf
roger <igor.onyshchenko@gmail.com>
2012-09-25 17:14:32 +0300

    i

--- data/aticleOrder.py
+++ data/aticleOrder.py
@@ -1,6 +1,7 @@
 articleOrder = [
     'trip-to-kinburn-peninsula',
     'list-of-goods-of-my-first-trakking',
+    'chatyr-dag-yayla',
     'visit-bilhorod-dnistrovskyi',
     'national-archaeological-park-olbia',
     'one-day-in-odessa',
--- data/categories.py
+++ data/categories.py
@@ -5,6 +5,7 @@ categories = {
     'one-day-in-odessa': 'towns',
     'odessa-region-trains-timetable': 'information',
     'national-archaeological-park-olbia': 'information',
+    'chatyr-dag-yayla': 'information',
     'list-of-goods-of-my-first-trakking': 'trakking'
 }
 
