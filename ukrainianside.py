311ea32c76d835b908130d62684a491cf73f5b5f
roger <igor.onyshchenko@gmail.com>
2012-11-01 14:29:20 +0200

    i

--- ukrainianside.py
+++ ukrainianside.py
@@ -63,7 +63,7 @@ def error_page_default(status, message, traceback, version):
     req = urllib.request.Request('http://localhost:18404/sendmail')
     req.add_header('Content-Type', 'application/x-www-form-urlencoded;charset=utf-8')
     res = urllib.request.urlopen(req, d)
-    return str(res.read())
+    return res.read().encode('utf-8')
 
 cherrypy.tree.mount(ukrainianside())
 
