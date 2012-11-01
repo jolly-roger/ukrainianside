fd9adc919b3c96acf97cfbdcb9d94e3a346e2f4c
roger <igor.onyshchenko@gmail.com>
2012-11-01 13:58:51 +0200

    i

--- ukrainianside.py
+++ ukrainianside.py
@@ -63,7 +63,7 @@ def error_page_default(status, message, traceback, version):
     req = urllib.request.Request('http://localhost:18404/sendmail')
     req.add_header('Content-Type', 'application/x-www-form-urlencoded;charset=utf-8')
     res = urllib.request.urlopen(req, d)
-    #resdata = res.read()
+    resdata = res.read()
     #return type(resdata)
     
     return 'Error'
